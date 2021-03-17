fd6780daf223f6b251a2969bcdf3306fd0e49895
Seyyid Ahmed Doğan <seyahdoo@gmail.com>
2021-03-17 12:54:22 +0300

    change hash method to blake3

--- hash_module.py
+++ hash_module.py
@@ -1,9 +1,10 @@
-﻿import hashlib
+﻿from blake3 import blake3
+
 
 def hash_file(path):
-    sha256_hash = hashlib.sha256()
+    hasher = blake3()
     with open(path,"rb") as f:
         for byte_block in iter(lambda: f.read(4096),b""):
-            sha256_hash.update(byte_block)
-        hash_string = sha256_hash.hexdigest()
-        return hash_string
+            hasher.update(byte_block)
+    hash_string = hasher.hexdigest()
+    return hash_string
