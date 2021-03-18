b7a5714ca5001ec24390821f47f9d2b0bc396097
Seyyid Ahmed Doğan <seyahdoo@gmail.com>
2021-03-18 20:52:48 +0300

    refactor

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import pymongo
 import re
 from send2trash import send2trash
 
-
 def hash_file(path):
     hasher = blake3()
     with open(path,"rb") as f:
@@ -17,9 +16,9 @@ def calculate_missing_hashes(collection):
     unhashed_documents = collection.find({"hash": None})
     for document in unhashed_documents:
         path = document["path"]
-        print(f"hashing {path}")
+        print(f"hashing   {path}")
         hash_string = hash_file(path)
-        print(f"hash:{path}:{hash_string}")
+        print(f"hash      {path}     {hash_string}")
         collection.update_one({"_id": document["_id"]}, { "$set": { "hash": hash_string } })
     return
 
@@ -55,14 +54,13 @@ def find_and_delete_dupes(source_collection, target_collection):
         target_path = target["path"]
         if not os.path.exists(target_path):
             print(f"removing from db {target_path} {target_filename}")
-            target_collection.delete_one({"_id": target["_id"]})    
-            
+            target_collection.delete_one({"_id": target["_id"]})
         is_dupe = source_collection.count_documents({"hash": target_hash, "filename": target_filename}, limit=1) != 0
         if is_dupe:
             print(f"dupe found {target_filename}           {target_path}         {target_hash}")
-            if os.path.exists(target_path):
-                send2trash(target_path)
-            target_collection.delete_one({"_id": target["_id"]})    
+            # if os.path.exists(target_path):
+            #     os.remove(target_path)
+            # target_collection.delete_one({"_id": target["_id"]})    
     return
 
 def delete_from_database_with_regex(collection):
@@ -76,22 +74,56 @@ def delete_from_database_with_regex(collection):
         collection.delete_one({"_id": target["_id"]})
     return
 
+def is_filename_image(filename):
+    return filename.endswith(".jpg") or \
+           filename.endswith(".png") or \
+           filename.endswith(".JPG") or \
+           filename.endswith(".PNG") or \
+           filename.endswith(".webm") or \
+           filename.endswith(".bmp") or \
+           filename.endswith(".mp3") or \
+           filename.endswith(".wav") or \
+           filename.endswith(".jpeg") or \
+           filename.endswith(".MOV") or \
+           filename.endswith(".mp4")
+
 def find_dupes_in_same_collection(collection):
-    findings = collection.aggregate([
-        {"$group" : { "_id": "$hash", "count": { "$sum": 1 } } },
-        {"$match": {"_id" :{ "$ne" : None } , "count" : {"$gt": 1} } },
-        {"$project": {"hash" : "$_id", "_id" : 0} }
-    ])
-    
-    # f = collection.find({"$group": { "_id": "$hash", "count": { "$sum": 1}}}, {"$match": { "count": { "$gt": 1 }}})
+    pipeline = [
+        {"$group": {"_id": { "hash": "$hash", "filename": "$filename"}, "count": {"$sum": 1}}},
+        {"$match": {"count": {"$gt": 1}}}
+    ]
+    findings = collection.aggregate(pipeline)
     for document in findings:
-        found_documents = collection.find({"hash": document["hash"]})
-        for found_document in found_documents:
-            print(found_document["path"])
-        print("--")
+        hash = document["_id"]["hash"]
+        filename:str = document["_id"]["filename"]
+        if "SquareHome2_backups" in filename:
+            continue
+            
+        # if filename.endswith(".txt"):
+        #     continue
+            
+        # if filename.endswith(".txt") or filename.endswith(".ini") or filename.endswith(".js") or filename.endswith(".pdf"):
+        #     continue
+            
+        if not is_filename_image(filename):
+            continue
+            
+        found_documents = collection.find({"hash": hash, "filename": filename})
+        count = found_documents.count()
+        if count > 0:
+            for found_document in found_documents:
+                print(f"{found_document['filename']}            {found_document['path']}")
+            print("--")
+            print(count)
     return
 
+def clean_collection(collection):
+    collection.delete_many({})
+    return 
+
 def do_job(source_collection, target_collection, source_path, target_path):
+    clean_collection(source_collection)
+    clean_collection(target_collection)
     sync_index_with_path(source_collection, source_path)
     sync_index_with_path(target_collection, target_path)
     find_and_delete_dupes(source_collection, target_collection)
@@ -109,9 +141,13 @@ def main():
     # do_job(
     #     temporary_source_collection, 
     #     temporary_target_collection, 
-    #     "U:/COLD_STORAGE/Terasad/Backups/Backup 05.08.2017/Records/Fantazya2017/", 
-    #     "U:/COLD_STORAGE/Terasad/Backups/Backup 15.09.2017/Photos/miCam/"
+    #     "U:/COLD_STORAGE/Seaboss/CDLERDEN/muhfoto2/", 
+    #     "U:/COLD_STORAGE/Seaboss/CDLERDEN/muhfoto1/"
     # )
+    
+    low_priority_paths = [
+        "U:/COLD_STORAGE/Seaboss/CDLERDEN/cd2/121CASIO/"
+    ]
     
     # sync_index_with_path(source_collection, "U:/COLD_STORAGE")
     find_dupes_in_same_collection(source_collection)
